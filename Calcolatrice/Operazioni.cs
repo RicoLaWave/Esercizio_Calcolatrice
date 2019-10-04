147aba11a3efcc7db6c3f1aa9438af82c4472747
RicoLaWave <56152468+RicoLaWave@users.noreply.github.com>
2019-10-04 12:01:14 +0200

    First commit of sourcecode

---
+++ Calcolatrice/Operazioni.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCalcolatrice;
+using MyInserimento;
+using MyOperazioni;
+
+
+namespace MyOperazioni
+{
+    class Operazioni
+    {
+
+        public static int InserisciNum()
+        {
+            int x = 0;
+            string numero;
+            bool successo;
+            do
+            {
+                Console.WriteLine("Caro Utente, inserisci il num e premi invio");
+                string numero1 = Console.ReadLine();
+                successo = Int32.TryParse(numero1, out x);
+
+                if (successo == false)
+                {
+                    Console.WriteLine("Il valore inserito non è corretto! RIPROVARE!");
+                }
+            }
+            while (successo == false);
+            return x;
+        }
+    }
+
+
+
+
+
+}
+}
---
+++ Calcolatrice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyOperazioni;
+using MyInserimento;
+
+namespace MyCalcolatrice
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            int op;
+            Console.WriteLine("Benvenuto in UMANA CALCULATOR!");
+
+            int num1 = Operazioni.InserisciNum();
+            Console.WriteLine(num1);
+            int num2 = Operazioni.InserisciNum();
+            Console.WriteLine(num2);
+
+       
+            op = Inserimento.InsertOperazioni(num1,num2);
+   
+            Console.WriteLine("Il risultato è:" +op);
+            Console.ReadLine();
+        }
+    }
+}
